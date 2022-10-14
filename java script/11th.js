5c8c5f601804f2a58d99e713b938777e402e65f6
mikellebonny <115636898+mikellebonny@users.noreply.github.com>
2022-10-14 12:20:54 +0100

    Add files via upload

---
+++ java script/11th.js
@@ -0,0 +1,37 @@
+// const jssone=["David peter",67,"david",false,"samdave", 70,"john doe", true]
+
+// const scores=[34,67,50,77,8656,90,86]
+// const condition = [ true, false,false,true,true]
+// console.log(jssone)
+// console.log(jssone[4])
+// jssone.pop(jssone)
+
+// jssone.push("maudlyn")
+// jssone.shift()
+// jssone.unshift("buhari")
+// jssone.includes("john doe")
+// console.log(jssone.includes("john doe"));
+// const myDog= {
+//     name: "yolly",
+//     age: 5
+// }
+
+const indigene = {
+  name: "maudlyn",
+  community: "bonny",
+  age: 67,
+  complexion: "brown",
+  state: "rivers",
+  nationality: "Nigeria",
+  children: {
+    firstchild: ["handy", "evelyn", "marry"],
+    secondchild: ["morris", "idris", "anderson"],
+    lastchild: ["marvellous", "mimi", "jalingo"],
+  },
+};
+console.log(indigene);
+console.log(indigene.state);
+console.log(indigene.complexion);
+console.log(indigene.children);
+console.log(indigene.children);
+console.log(indigene.children.firstchild[1]);
---
+++ java script/index.js
@@ -0,0 +1,42 @@
+// var myname ="dad"
+// var pp= 100+300
+// var ismarried=true
+
+
+// console.log(typeof ismarried);
+// var myvision="To build a world class school"
+//  console.log(myvision);
+
+
+//  var myhome="Riverstate"
+//  console.log(myhome);
+
+//  var myidentity="am a mum"
+//  console.log(myidentity);
+
+//  var pp= 100000000 + 3 * 52 
+
+//  console.log(pp)
+//   var ourworldis=true
+
+//   console.log(ourworldis);
+
+//   var pp=334+1000000
+//   console.log(pp)
+  
+const istodaymayday = false
+console.log(istodaymayday);
+
+const whatismylocation= "PORTHARCOURT"
+console.log(whatismylocation.length);
+
+console.log(whatismylocation.toLowerCase());
+console.log(whatismylocation.toUpperCase());
+console.log(whatismylocation.startsWith("R"));
+console.log(whatismylocation.endsWith("t"));
+console.log(whatismylocation.includes("h"));
+console.log(whatismylocation.trim());
+ 
+
+ 
+ 
